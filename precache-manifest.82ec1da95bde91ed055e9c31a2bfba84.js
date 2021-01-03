acce28775db5d2396511dd7dde5de8d9c4f8bbfc
krishkcompanysec <65386127+krishkcompanysec@users.noreply.github.com>
2021-01-03 15:37:00 +0530

    Add files via upload

---
+++ precache-manifest.82ec1da95bde91ed055e9c31a2bfba84.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/counter-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "88bf44f0f2b26945b975",
+    "url": "/counter-app/static/css/main.3e825baf.chunk.css"
+  },
+  {
+    "revision": "0f1f037b32656fd93884",
+    "url": "/counter-app/static/js/runtime~main.c63f0888.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/counter-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "8982d7f9d79fa5647b3c",
+    "url": "/counter-app/static/js/2.cdc130cc.chunk.js"
+  },
+  {
+    "revision": "88bf44f0f2b26945b975",
+    "url": "/counter-app/static/js/main.32d87ae3.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/counter-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/counter-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/counter-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "8982d7f9d79fa5647b3c",
+    "url": "/counter-app/static/css/2.5b79855f.chunk.css"
+  },
+  {
+    "revision": "e5be14f74a61afd911a0c193150911ee",
+    "url": "/counter-app/index.html"
+  }
+];
